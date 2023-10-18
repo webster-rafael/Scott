9e55aec546d73af58c8e32e0f8d143bdebed5ede
Webster Dev <webster.rafael2014@gmail.com>
2023-10-18 01:24:18 -0300

    NEW

--- script.js
+++ script.js
@@ -24,6 +24,11 @@ window.sr = ScrollReveal({ reset: true});
     distance: '200px', // distância do deslocamento
     delay: 200 });
 
+    ScrollReveal().reveal('.lista5', { duration: 2000, // duração da animação em milissegundos
+    origin: 'right', // de onde a animação deve vir ('left' para da esquerda para a direita)
+    distance: '200px', // distância do deslocamento
+    delay: 200 });
+
     ScrollReveal().reveal('.title', { duration: 2000, // duração da animação em milissegundos
     origin: 'top', // de onde a animação deve vir ('left' para da esquerda para a direita)
     distance: '200px', // distância do deslocamento
