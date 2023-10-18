1c617d987a08faaa3ed87f58d96ab092beae9e2c
Webster Dev <webster.rafael2014@gmail.com>
2023-10-17 23:58:33 -0300

    NEW

---
+++ script.js
@@ -0,0 +1,40 @@
+window.sr = ScrollReveal({ reset: true});
+    sr.reveal('.profile', {
+         duration: 2000,
+         rotate: {x:0, y: 80, z:0}
+    });
+
+    ScrollReveal().reveal('.lista1', { duration: 2000, // duração da animação em milissegundos
+    origin: 'right', // de onde a animação deve vir ('left' para da esquerda para a direita)
+    distance: '200px', // distância do deslocamento
+    delay: 200 });
+
+    ScrollReveal().reveal('.lista2', { duration: 2000, // duração da animação em milissegundos
+    origin: 'left', // de onde a animação deve vir ('left' para da esquerda para a direita)
+    distance: '200px', // distância do deslocamento
+    delay: 200 });
+
+    ScrollReveal().reveal('.lista3', { duration: 2000, // duração da animação em milissegundos
+    origin: 'right', // de onde a animação deve vir ('left' para da esquerda para a direita)
+    distance: '200px', // distância do deslocamento
+    delay: 200 });
+
+    ScrollReveal().reveal('.lista4', { duration: 2000, // duração da animação em milissegundos
+    origin: 'left', // de onde a animação deve vir ('left' para da esquerda para a direita)
+    distance: '200px', // distância do deslocamento
+    delay: 200 });
+
+    ScrollReveal().reveal('.title', { duration: 2000, // duração da animação em milissegundos
+    origin: 'top', // de onde a animação deve vir ('left' para da esquerda para a direita)
+    distance: '200px', // distância do deslocamento
+    delay: 200 });
+
+    ScrollReveal().reveal('.social1', { duration: 2000, // duração da animação em milissegundos
+    origin: 'right', // de onde a animação deve vir ('left' para da esquerda para a direita)
+    distance: '200px', // distância do deslocamento
+    delay: 200 });
+
+    ScrollReveal().reveal('.social2', { duration: 2000, // duração da animação em milissegundos
+    origin: 'left', // de onde a animação deve vir ('left' para da esquerda para a direita)
+    distance: '200px', // distância do deslocamento
+    delay: 200 });
